c94503567b7b60ca5e11dc0b2c0e58f08816b7c4
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-09 21:05:08 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
-  #squares = []
-  array.each { |x| x *= x }
+  squares = []
+  array.each { |x| squares.push(x*x) }
   #array.collect {|x| x*x}
-  #squares
+  squares
 end
